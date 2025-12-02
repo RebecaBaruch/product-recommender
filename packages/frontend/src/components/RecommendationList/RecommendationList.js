d76534a983aed120423533f09c1bba5c268b9227
Rebeca Baruch <rebecabaruch@outlook.com>
2025-12-02 17:59:03 -0300

    fix: styled RecommendationList

--- packages/frontend/src/components/RecommendationList/RecommendationList.js
+++ packages/frontend/src/components/RecommendationList/RecommendationList.js
@@ -2,18 +2,14 @@
 import React from 'react';
 
 function RecommendationList({ recommendations }) {
-  <h2 className="text-xl font-bold mb-4 text-gray-800">
-    Lista de Recomendações:
-  </h2>;
-
   // Se a lista estiver vazia, retorna a mensagem
   if (!recommendations || recommendations.length === 0) {
     return (
       <div>
-        <h2 className="text-xl font-bold mb-4 text-gray-800">
+        <h2 className="text-md md:text-xl font-bold mb-4 text-gray-800">
           Lista de Recomendações:
         </h2>
-        <div className="p-4 bg-yellow-50 border-l-4 border-yellow-400 text-yellow-800 rounded">
+        <div className="p-4 bg-yellow-50 border-l-4 border-yellow-400 text-sm md:text-md text-yellow-800 rounded">
           <p className="font-medium">
             Nenhuma recomendação encontrada. Selecione mais opções para melhores
             resultados.
@@ -26,7 +22,7 @@ function RecommendationList({ recommendations }) {
   // Se houver recomendações, exiba a lista formatada
   return (
     <div>
-      <h2 className="text-xl font-bold mb-4 text-gray-800">
+      <h2 className="text-md md:text-xl font-bold mb-4 text-gray-800">
         Lista de Recomendações:
       </h2>
 
@@ -36,15 +32,15 @@ function RecommendationList({ recommendations }) {
             key={product.id || product.name}
             className="p-5 border-l-4 border-green-600 rounded-lg shadow-md bg-green-50 transition-shadow hover:shadow-lg"
           >
-            <p className="font-extrabold text-xl text-green-800 mb-1">
+            <p className="font-extrabold text-sm md:text-xl text-green-800 mb-1">
               {product.name}
             </p>
-            <p className="text-sm text-green-600">
+            <p className="text-xs md:text-sm text-green-600">
               <b>Categoria:</b> {product.category}
             </p>
 
             {product.score !== undefined && (
-              <p className="text-xs text-green-600 mt-1">
+              <p className="text-xs md:text-sm text-green-600 mt-1">
                 <b>Pontuação de Match:</b> {product.score}
               </p>
             )}
